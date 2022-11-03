74c53506c890512402a8459984c78875eceb9265
Rαjαt Ҝαsђyαp <50959267+STRK-ND@users.noreply.github.com>
2022-11-04 01:03:46 +0530

    install few necessary packages

--- build.sh
+++ build.sh
@@ -76,6 +76,21 @@ DEFCONFIG=X00TD_defconfig
 # Specify compiler. 
 # 'clang' or 'gcc'
 COMPILER=clang
+	if [ $COMPILER = "clang" ]
+	then
+		# install few necessary packages
+		apt-get update && apt-get install     
+		adb autoconf automake axel bc bison build-essential
+                ccache clang cmake curl expat fastboot flex g++
+                g++-multilib gawk gcc gcc-multilib git gnupg gperf
+                htop imagemagick lib32ncurses5-dev lib32z1-dev libtinfo5 libc6-dev libcap-dev
+                libexpat1-dev libgmp-dev '^liblz4-.*' '^liblzma.*' libmpc-dev libmpfr-dev libncurses5-dev 
+                libsdl1.2-dev libssl-dev libtool libxml2 libxml2-utils '^lzma.*' lzop
+                maven ncftp ncurses-dev patch patchelf pkg-config pngcrush
+                pngquant python2.7 python-all-dev re2c schedtool squashfs-tools subversion
+                texinfo unzip w3m xsltproc zip zlib1g-dev lzip 
+                libxml-simple-perl libswitch-perl apt-utils -y
+	fi
 
 # Build modules. 0 = NO | 1 = YES
 MODULES=0
