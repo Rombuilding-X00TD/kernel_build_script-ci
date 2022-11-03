a62419417f71bac4864d7de721552ec1c1e5c395
Rαjαt Ҝαsђyαp <50959267+STRK-ND@users.noreply.github.com>
2022-11-04 01:13:47 +0530

    Added akhilnarang/scripts for updating server

--- build.sh
+++ build.sh
@@ -79,17 +79,7 @@ COMPILER=clang
 	if [ $COMPILER = "clang" ]
 	then
 		# install few necessary packages
-		apt-get update && apt-get install     
-		adb autoconf automake axel bc bison build-essential
-                ccache clang cmake curl expat fastboot flex g++
-                g++-multilib gawk gcc gcc-multilib git gnupg gperf
-                htop imagemagick lib32ncurses5-dev lib32z1-dev libtinfo5 libc6-dev libcap-dev
-                libexpat1-dev libgmp-dev '^liblz4-.*' '^liblzma.*' libmpc-dev libmpfr-dev libncurses5-dev 
-                libsdl1.2-dev libssl-dev libtool libxml2 libxml2-utils '^lzma.*' lzop
-                maven ncftp ncurses-dev patch patchelf pkg-config pngcrush
-                pngquant python2.7 python-all-dev re2c schedtool squashfs-tools subversion
-                texinfo unzip w3m xsltproc zip zlib1g-dev lzip 
-                libxml-simple-perl libswitch-perl apt-utils -y
+git clone https://github.com/akhilnarang/scripts && cd scripts && bash setup/android_build_env.sh && cd
 	fi
 
 # Build modules. 0 = NO | 1 = YES
